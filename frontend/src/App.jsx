d2da30f880435d1109503991f13204fd9b477e65
Precious <preciousnyandega@gmail.com>
2025-11-29 01:01:46 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,8 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 // Example protected dashboard
 function Dashboard() {
